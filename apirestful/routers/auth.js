ccad107d66e7b85e1ae725aaeba583b8b9e3ff63
Arief Saifuddien <ariefsaifuddien01@gmail.com>
2022-12-01 00:58:03 +0200

    Delete apirestful directory

--- apirestful/routers/auth.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { register } = require('../controllers/auth.jsx');
-
-const authRoute = express.Router()
-
-authRoute.post('/register', register)
-
-module.exports = authRoute
